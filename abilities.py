a7be6d5d0696407ea0a3fed52c40c4154681440d
Marcus Grahn <marcus.grahn@hm.com>
2024-10-20 23:50:38 +0200

    used loggger instead of prints

--- abilities.py
+++ abilities.py
@@ -1,5 +1,6 @@
 from typing import Callable, Optional
 import random
+from logger import logging
 
 class Ability:
     def __init__(self, name: str, description: str, trigger: str, action: Callable[["Character", list["CharacterSlot"], list["CharacterSlot"]], None]) -> None:
@@ -16,13 +17,14 @@ class Ability:
 def volley(character: "Character", allies: list["CharacterSlot"], enemies: list["CharacterSlot"]) -> None:
     """Deals 1 damage to 2 random enemies at the start of combat."""
     #Working
+    logging.debug(f"{character.name} uses Volley")
     valid_targets = [slot.content for slot in enemies if slot.content and not slot.content.is_dead()]
     if valid_targets:
         for _ in range(2):
             if valid_targets:
                 target = random.choice(valid_targets)
                 target.damage_health(1)
-                print(f"{character.name} uses Volley on {target.name}, dealing 1 damage.")
+                logging.debug(f"{character.name} uses Volley on {target.name}, dealing 1 damage.")
 
 
 def assassinate(character: "Character", allies: list["CharacterSlot"], enemies: list["CharacterSlot"]) -> None:
@@ -51,7 +53,7 @@ def heal(character: "Character", allies: list["CharacterSlot"], enemies: list["C
     if valid_targets:
         lowest_health_ally = min(valid_targets, key=lambda ally: ally.health)
         lowest_health_ally.health = min(lowest_health_ally.health + 1, lowest_health_ally.max_health)
-        print(f"{character.name} uses Heal on {lowest_health_ally.name}, healing 1 health.")
+        logging.debug(f"{character.name} uses Heal on {lowest_health_ally.name}, healing 1 health.")
 
 
 def blast(character: "Character", allies: list["CharacterSlot"], enemies: list["CharacterSlot"]) -> None:
@@ -71,7 +73,7 @@ def rampage(character: "Character", allies: list["CharacterSlot"], enemies: list
     """Gains 1 attack when attacking."""
     #Working
     character.damage += 1
-    print(f"{character.name} uses Rampage, gaining 1 attack.")
+    logging.debug(f"{character.name} uses Rampage, gaining 1 attack.")
 
 def fortify(character: "Character", allies: list["CharacterSlot"], enemies: list["CharacterSlot"]) -> None:
     """Increases all allies' health by 1 at the start of combat."""
@@ -81,4 +83,4 @@ def reckless(character: "Character", allies: list["CharacterSlot"], enemies: lis
     """Loses 1 health when attacking."""
     #Working
     character.damage_health(1)
-    print(f"{character.name} uses Reckless, losing 1 health.")
+    logging.debug(f"{character.name} uses Reckless, losing 1 health.")
